fa99620b139154741b790cd99dd4b917afcf1004
laund <me@laund.moe>
2024-07-06 13:32:23 +0200

    fix render layers example

--- examples/render_layer.rs
+++ examples/render_layer.rs
@@ -7,7 +7,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(ScreenDiagnosticsPlugin {
-            render_layer: RenderLayers::default(),
+            render_layer: RenderLayers::from_layers(&[1]), // render diagnostics to different layer
             ..default()
         })
         .add_plugins(ScreenFrameDiagnosticsPlugin)
@@ -17,5 +17,7 @@ fn main() {
 
 // need a camera to display the UI
 fn setup_camera(mut commands: Commands) {
+    // spawn camera on different layer
     commands.spawn((Camera2dBundle::default(), RenderLayers::from_layers(&[1])));
+    // could be useful for rendre-to-texture or editor-like applications
 }
